49cad1090a61f0e7ebd045dd5cf82b78941747ca
Marvel Li <siqi_li@mymail.sutd.edu.sg>
2024-06-03 20:53:13 +0800

    Update ConvexHullExample

--- Examples/Source/ConvexHullExample.h
+++ Examples/Source/ConvexHullExample.h
@@ -11,9 +11,12 @@ inline auto ConvexHullExample()
 {
 	return [](World& World) {
 		auto Rabbit = StaticMesh::LoadObj( "stanford-bunny.obj");
+		Rabbit->RotateEuler({M_PI_2, 0., 0.});
+		Rabbit->Scale(2.);
 		auto ConvexHull = Math::ConvexHull(Rabbit);
+		ConvexHull->Scale(1.001); // Avoid z-fighting
 		ConvexHull->GetMaterial()->SetBaseColor({ 0.5, 0, 0 });
-		ConvexHull->GetMaterial()->SetAlpha(0.3);
+		ConvexHull->GetMaterial()->SetAlpha(0.5);
 		ConvexHull->GetMaterial()->SetShowWireframe(true);
 		World.SpawnActor<StaticMeshActor>("Rabbit", Rabbit);
 		World.SpawnActor<StaticMeshActor>("ConvexHull", ConvexHull);
