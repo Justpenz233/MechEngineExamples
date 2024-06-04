d0eb2dba1a50043cbf1cd8addb98db722e5c5e67
Marvel Li <siqi_li@mymail.sutd.edu.sg>
2024-06-04 14:56:53 +0800

    Update MetaDataExample.h

--- Examples/Source/MetaDataExample.h
+++ Examples/Source/MetaDataExample.h
@@ -6,29 +6,62 @@
 #include "Log/Log.h"
 #include "Object/Object.h"
 #include "Game/World.h"
-MCLASS(TestMetadataClass)
-class TestMetadataClass : public Object
+#include "Game/StaticMeshActor.h"
+#include "Mesh/BasicShapesLibrary.h"
+
+MCLASS(MyCylinder)
+class MyCylinder : public StaticMeshActor
 {
-	REFLECTION_BODY(TestMetadataClass)
-	TestMetadataClass() = default;
+	REFLECTION_BODY(MyCylinder)
+	MyCylinder() = default;
+
+public:
+	// Init the cylinder
+	virtual void Init() override
+	{
+		StaticMeshActor::Init();
+		GetStaticMeshComponent()->SetMeshData(BasicShapesLibrary::GenerateCylinder(Length, Radius, Samples));
+	}
 
 protected:
-	MPROPERTY()
-	int TestInt = 0;
+	MPROPERTY(Slide_(0.1f, 1.f), Category_("MyProperty"))
+	float Radius = 0.3f;
 
-	MPROPERTY(Drag_(0.f, 1.f), Category_("Test"))
-	float TestFloat = 0.0f;
+	MPROPERTY(Step_(0.5f, 2.f), Category_("MyProperty"))
+	double Length = 1.f;
+
+	MPROPERTY(Slide_(4, 64), Category_("MyProperty"))
+	int Samples = 8;
+
+	// When the property is edited, update the mesh data
+	virtual void PostEdit(Reflection::FieldAccessor& Field) override
+	{
+		if (Field == NAME(Radius) || Field == NAME(Length) || Field == NAME(Samples))
+			GetStaticMeshComponent()->SetMeshData(BasicShapesLibrary::GenerateCylinder(Length, Radius, Samples));
+	}
 };
 
 inline auto MetaDataExample()
 {
-	return [](::World& World)
+	return [](World& World)
 	{
-		auto Object = NewObject<TestMetadataClass>();
+		auto Object = World.SpawnActor<MyCylinder>("MyCylinder");
 
+		// Iterate all properties
 		for(auto Property : Object->GetAllPropertyAceessors())
 		{
 			MechEngine::LOG_TEMP("Property type:{} name:{}", Property.getTypeName(), Property.getFieldName());
+			for (auto Tag : Property.GetPropertyTags())
+			{
+				if (auto Slide = dynamic_cast<const Slide_*>(Tag))
+				{
+					MechEngine::LOG_TEMP("Drag tag, min:{} max:{}", Slide->GetMin(), Slide->GetMax());
+				}
+				if (auto Category = dynamic_cast<const Category_*>(Tag))
+				{
+					MechEngine::LOG_TEMP("Category tag, category:{}", Category->ParseCategory()[0]);
+				}
+			}
 		}
 	};
 };
